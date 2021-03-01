28b7d480e8b6e6bd1cf8835d06e8643d36ec22d1
Lucas Lopatka <lucas@Lucass-MacBook-Pro.local>
2021-03-02 02:04:25 +0700

    Comment out any migrations that require GIS extensions so we can run parts of the API that work.

--- migration/1613577521127-initialize-tables.ts
+++ migration/1613577521127-initialize-tables.ts
@@ -1,5 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from 'typeorm';
 
+// Code tables
 import {
     createTable as createAttachmentTypeCodeTable,
     dropTable as dropAttachmentTypeCodeTable
@@ -17,6 +18,40 @@ import {
     dropTable as dropWorkflowStateCodeTable
 } from './1613577521127-initialize-tables/workflow-state-code.table';
 
+// Core tables
+import {
+    createTable as createAttachmentTable,
+    dropTable as dropAttachmentTable
+} from './1613577521127-initialize-tables/attachment.table';
+import {
+    createTable as createCutBlockTable,
+    dropTable as dropCutBlockTable
+} from './1613577521127-initialize-tables/cut-block.table';
+import {
+    createTable as createInteractionTable,
+    dropTable as dropInteractionTable
+} from './1613577521127-initialize-tables/interaction.table';
+import {
+    createTable as createProjectTable,
+    dropTable as dropProjectTable
+} from './1613577521127-initialize-tables/project.table';
+import {
+    createTable as createPublicCommentTable,
+    dropTable as dropPublicCommentTable
+} from './1613577521127-initialize-tables/public-comment.table';
+import {
+    createTable as createRetentionAreaTable,
+    dropTable as dropRetentionAreaTable
+} from './1613577521127-initialize-tables/retention-area.table';
+import {
+    createTable as createRoadSectionTable,
+    dropTable as dropRoadSectionTable
+} from './1613577521127-initialize-tables/road-section.table';
+import {
+    createTable as createSubmissionTable,
+    dropTable as dropSubmissionTable
+} from './1613577521127-initialize-tables/submission.table';
+
 export class initializeTables1613577521127 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
@@ -25,6 +60,15 @@ export class initializeTables1613577521127 implements MigrationInterface {
         createResponseCodeTable(queryRunner);
         createSubmissionTypeCodeTable(queryRunner);
         createWorkflowStateCodeTable(queryRunner);
+        // Create core tables
+        createAttachmentTable(queryRunner);
+        // createCutBlockTable(queryRunner);
+        createInteractionTable(queryRunner);
+        createProjectTable(queryRunner);
+        createPublicCommentTable(queryRunner);
+        // createRetentionAreaTable(queryRunner);
+        // createRoadSectionTable(queryRunner);
+        // createSubmissionTable(queryRunner);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -33,5 +77,14 @@ export class initializeTables1613577521127 implements MigrationInterface {
         dropResponseCodeTable(queryRunner);
         dropSubmissionTypeCodeTable(queryRunner);
         dropWorkflowStateCodeTable(queryRunner);
+        // Drop core tables
+        dropAttachmentTable(queryRunner);
+        // dropCutBlockTable(queryRunner);
+        dropInteractionTable(queryRunner);
+        dropProjectTable(queryRunner);
+        dropPublicCommentTable(queryRunner);
+        // dropRetentionAreaTable(queryRunner);
+        // dropRoadSectionTable(queryRunner);
+        // dropSubmissionTable(queryRunner);
     }
 }
