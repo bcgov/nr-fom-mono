50d8a6878f23904ee7f5fbee491a80b876da6a20
Lucas Lopatka <lucas@Lucass-MacBook-Pro.local>
2021-03-16 09:02:54 +0700

    Fix foreign key generation TypeORM's createForeignKey doesn't seem to be working use raw queries.

--- migration/1613577521127-initialize-tables.ts
+++ migration/1613577521127-initialize-tables.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner} from 'typeorm';
+import {MigrationInterface, QueryRunner, TableForeignKey} from 'typeorm';
 
 // Code tables
 import {
@@ -56,19 +56,147 @@ export class initializeTables1613577521127 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // Create code tables first
-        createAttachmentTypeCodeTable(queryRunner);
-        createResponseCodeTable(queryRunner);
-        createSubmissionTypeCodeTable(queryRunner);
-        createWorkflowStateCodeTable(queryRunner);
+        await createAttachmentTypeCodeTable(queryRunner);
+        await createResponseCodeTable(queryRunner);
+        await createSubmissionTypeCodeTable(queryRunner);
+        await createWorkflowStateCodeTable(queryRunner);
         // Create core tables
-        createAttachmentTable(queryRunner);
-        createCutBlockTable(queryRunner);
-        createInteractionTable(queryRunner);
-        createProjectTable(queryRunner);
-        createPublicCommentTable(queryRunner);
-        createRetentionAreaTable(queryRunner);
-        createRoadSectionTable(queryRunner);
-        createSubmissionTable(queryRunner);
+        await createAttachmentTable(queryRunner);
+        await createCutBlockTable(queryRunner);
+        await createInteractionTable(queryRunner);
+        await createProjectTable(queryRunner);
+        await createPublicCommentTable(queryRunner);
+        await createRetentionAreaTable(queryRunner);
+        await createRoadSectionTable(queryRunner);
+        await createSubmissionTable(queryRunner);
+
+        // Create foreign keys
+        const foreignKeys = {
+            'attachment': [
+                {
+                    columnNames: ['project_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'project',
+                    // onDelete: 'CASCADE'
+                },
+                {
+                    columnNames: ['attachment_type_code'],
+                    referencedColumnNames: ['code'],
+                    referencedTableName: 'attachment_type_code',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'cut_block': [
+                {
+                    columnNames: ['submission_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'submission',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'interaction': [
+                {
+                    columnNames: ['project_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'project',
+                    // onDelete: 'CASCADE'
+                },
+                {
+                    columnNames: ['attachment_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'attachment',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'project': [
+                /* {
+                    columnNames: ['fsp_id'],
+                    referencedColumnNames: ['fsp_id'],
+                    referencedTableName: 'fsp',
+                    // onDelete: 'CASCADE'
+                }, */
+                /* {
+                    columnNames: ['district_id'],
+                    referencedColumnNames: ['district_id'],
+                    referencedTableName: 'district',
+                    // onDelete: 'CASCADE'
+                }, */
+                /* {
+                    columnNames: ['forest_client_id'],
+                    referencedColumnNames: ['forest_client_id'],
+                    referencedTableName: 'forest_client',
+                    // onDelete: 'CASCADE'
+                }, */
+                {
+                    columnNames: ['workflow_state_code'],
+                    referencedColumnNames: ['code'],
+                    referencedTableName: 'workflow_state_code',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'public_comment': [
+                {
+                    columnNames: ['project_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'project',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'retention_area': [
+                {
+                    columnNames: ['submission_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'submission',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'road_section': [
+                {
+                    columnNames: ['submission_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'submission',
+                    // onDelete: 'CASCADE'
+                }
+            ],
+            'submission': [
+                {
+                    columnNames: ['project_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'project',
+                    // onDelete: 'CASCADE'
+                },
+                {
+                    columnNames: ['submission_type_code'],
+                    referencedColumnNames: ['code'],
+                    referencedTableName: 'submission_type_code',
+                    // onDelete: 'CASCADE'
+                }
+            ]
+        }
+
+        Object.keys(foreignKeys).map(async (tableName) => {
+            const columns = foreignKeys[tableName];
+            console.log(`generating foreign keys for table: ${tableName}`);
+            columns.map(async (col) => {
+                const name = `fk_${tableName}_${col.columnNames.join('_')}`;
+                /* const foreignKey = new TableForeignKey({
+                    columnNames: col.columnNames,
+                    referencedColumnNames: col.referencedColumnNames,
+                    referencedTableName: col.referencedTableName,
+                    // onDelete: col.onDelete
+                    name: name
+                }); */
+                console.log(`key ${name} for table: ${tableName}`);
+
+                let fkQuery = `ALTER TABLE ${tableName} ADD CONSTRAINT ${name}`;
+                fkQuery += ` FOREIGN KEY (${col.columnNames})`;
+                fkQuery += ` REFERENCES ${col.referencedTableName}(${col.referencedColumnNames.join(', ')})`;
+                console.log(fkQuery);
+
+                // const result = await queryRunner.createForeignKey(tableName, foreignKey);
+                await queryRunner.query(fkQuery);
+            });
+        });
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
