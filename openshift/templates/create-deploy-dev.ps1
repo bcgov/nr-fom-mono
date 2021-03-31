0df76d26211e7681606492e42c1c8fb098d7ccbd
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-03-31 09:47:00 -0600

    Address review comments on openshift setup

--- openshift/templates/create-deploy-dev.ps1
+++ openshift/templates/create-deploy-dev.ps1
@@ -2,4 +2,4 @@ Set-Variable -Name "name" -Value "api"
 Set-Variable -Name "tag" -Value "main"
 Set-Variable -Name "env" -Value "dev"
 oc delete all,NetworkPolicy -n a4b31c-$env -l template=fom-$name-deploy,tag=$tag
-oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag | oc create -n a4b31c-$env -f -
+oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag -p HOSTNAME="nr-fom-$name-$tag-$env" | oc create -n a4b31c-$env -f -
