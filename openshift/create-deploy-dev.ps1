5bef7c77aa22899a15b5abf2d7814fe03df03da8
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-04-09 09:07:42 -0600

    Fix dockerfile

--- openshift/create-deploy-dev.ps1
+++ openshift/create-deploy-dev.ps1
@@ -3,3 +3,8 @@ Set-Variable -Name "tag" -Value "main"
 Set-Variable -Name "env" -Value "dev"
 oc delete all,NetworkPolicy -n a4b31c-$env -l template=fom-$name-deploy,tag=$tag
 oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag -p HOSTNAME="nr-fom-$name-$tag-$env" | oc create -n a4b31c-$env -f -
+
+Set-Variable -Name "tag" -Value "dev"
+Set-Variable -Name "env" -Value "dev"
+oc delete all,NetworkPolicy -n a4b31c-$env -l template=fom-$name-deploy,tag=$tag
+oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag -p HOSTNAME="nr-fom-$name-$tag-$env" | oc create -n a4b31c-$env -f -
