d3f94204b48767f4a4a1ad8bbe9843dacc492002
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2021-05-19 11:00:12 -0700

    Add few nest class-validators for POSTing public comment basic validations.

--- apps/api/src/app/modules/public-comment/dto/public-comment.dto.ts
+++ apps/api/src/app/modules/public-comment/dto/public-comment.dto.ts
@@ -3,35 +3,59 @@ import { BaseDto } from '@dto';
 import { Project } from '../../project/entities/project.entity';
 import { ResponseCode } from '../../response-code/entities/response-code.entity';
 import { CommentScopeCode } from '../../comment-scope-code/entities/comment-scope-code.entity';
+import { IsEmail, IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
 
 export class PublicCommentDto extends BaseDto {
+
+  @MaxLength(4000)
   @ApiProperty()
   feedback: string;
+
+  @IsOptional()
+  @MaxLength(50)
   @ApiProperty()
   name: string;
+
+  @IsOptional()
+  @MaxLength(50)
   @ApiProperty()
   location: string;
+
+  @IsOptional()
+  @IsEmail()
   @ApiProperty()
   email: string;
+
+  @IsOptional()
   @ApiProperty()
   phoneNumber: string;
+
   @ApiProperty()
   responseDetails: string;
-  // Relationships
+
+  @IsNotEmpty()
   @ApiProperty()
   projectId: number;
+
   @ApiProperty()
   project: Project;
+
   @ApiProperty()
   responseCode: string;
+
   @ApiProperty()
   response: ResponseCode;
+
+  @IsNotEmpty()
   @ApiProperty()
   commentScopeCode: string;
+
   @ApiProperty()
   commentScope: CommentScopeCode;
+
   @ApiProperty()
   scopeCutBlockId: number;
+
   @ApiProperty()
   scopeRoadSectionId: number;
 }
