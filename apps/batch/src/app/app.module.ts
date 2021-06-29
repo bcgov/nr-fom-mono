def4a9cf26c97cbf8a0479b925abda48cbe56ed8
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-06-29 14:53:42 -0600

    Batch proof of concept to query database.

--- apps/batch/src/app/app.module.ts
+++ apps/batch/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 
-import { AppService } from './app.service';
+import { BatchService } from './batch.service';
 import { LoggerModule } from 'nestjs-pino';
 
 import { AppConfigModule } from '@api-modules/app-config/app-config.module';
 import { AppConfigService } from '@api-modules/app-config/app-config.provider';
-import { ProjectModule } from '@api-modules/project/project.module';
-import { SubmissionModule } from '@api-modules/submission/submission.module';
+import { Project } from '@api-modules/project/project.entity';
+import { WorkflowStateCode } from '@api-modules/project/workflow-state-code.entity';
+import { ProjectService } from '@api-modules/project/project.service';
+import { DistrictService } from '@api-modules/district/district.service';
+import { District } from '@api-modules/district/district.entity';
+import { ForestClientService } from '@api-modules/forest-client/forest-client.service';
+import { ForestClient } from '@api-modules/forest-client/forest-client.entity';
 
 function getLogLevel():string {
   return process.env.LOG_LEVEL || 'info';
@@ -43,15 +48,10 @@ function getLogLevel():string {
     }),
 
     // TODO: Don't want to import controllers into batch.
-    // TypeOrmModule.forFeature([Project, WorkflowStateCode]),
-
-    ProjectModule,
-    SubmissionModule,
-
+    TypeOrmModule.forFeature([Project, WorkflowStateCode, District, ForestClient]),
 
   ],
-  // controllers: [AppController],
-  providers: [AppService],
+  providers: [BatchService, ProjectService, DistrictService, ForestClientService],
   
 })
 export class AppModule {}
