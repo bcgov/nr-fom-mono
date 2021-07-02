8194d19e075a9077a2a4b81c22fb8fc5cdf3b859
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-07-02 16:02:39 -0600

    Add API config parameters.

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -17,22 +17,27 @@ module.exports = class {
     const name = 'fom-api'
   
     const propertiesByPhase = {
-      phase:        {build: 'build'                       , dev: 'dev'                          , test: 'test'                      , prod: 'prod'},
-      changeId:     {build: changeId                      , dev: changeId                       , test: changeId                    , prod: changeId},
-      namespace:    {build: `${namespacePrefix}-tools`    , dev: `${namespacePrefix}-dev`       , test: `${namespacePrefix}-test`   , prod: `${namespacePrefix}-prod`},
+      phase:            {build: 'build'                       , dev: 'dev'                          , test: 'test'                      , prod: 'prod'},
+      changeId:         {build: changeId                      , dev: changeId                       , test: changeId                    , prod: changeId},
+      namespace:        {build: `${namespacePrefix}-tools`    , dev: `${namespacePrefix}-dev`       , test: `${namespacePrefix}-test`   , prod: `${namespacePrefix}-prod`},
 
       // Suffix added to OpenShift resource names.
-      suffix:       {build: `-build-${changeId}`          , dev: `-dev-${changeId}`             , test: `-test`                     , prod: `-prod`  },
+      suffix:           {build: `-build-${changeId}`          , dev: `-dev-${changeId}`             , test: `-test`                     , prod: `-prod`  },
       // ImageStream tag
-      tag:          {build: `build-${version}-${changeId}`, dev: `dev-${version}-${changeId}`   , test: `test-${version}`           , prod: `prod-${version}`},
+      tag:              {build: `build-${version}-${changeId}`, dev: `dev-${version}-${changeId}`   , test: `test-${version}`           , prod: `prod-${version}`},
 
       // Same hostname needs to be used for the Admin and Public components. Also need to white-list these URLs for KeyCloak...
       // TODO: How is that going to work in dev with different change ids.
-      hostname:     {build: `n/a`                         , dev: `fom-nrs-dev-${changeId}`      , test: `fom-nrs-test`              , prod: `fom-nrs-prod`},
+      hostname:         {build: `n/a`                         , dev: `fom-nrs-dev-${changeId}`      , test: `fom-nrs-test`              , prod: `fom-nrs-prod`},
 
-      // TODO: Not sure what instance is used for.
-      instance:     {build: `${name}-build-${changeId}`   , dev: `${name}-dev-${changeId}`      , test: `${name}-test`              , prod: `${name}-prod-${changeId}`},
+      testDataEnabled:  {build: 'n/a'                         , dev: 'true'                         , test: 'true'                      , prod: 'false'},
+      // Consider having only 1 replica with test data enabled as populating the large volume test data can impact startup when there are multiple replicas.
+      apiReplicaCount:  {build: 'n/a'                         , dev: '1'                            , test: '2'                         , prod: '3'},
+      // TODO: Need to get test/prod hostnames whitelisted in Keycloak, and need to get dev enabled most likely.
+      keycloakEnabled:  {build: 'n/a'                         , dev: 'false'                        , test: 'true'                      , prod: 'true'},
     };
+
+    // DB_TESTDATA=$TestData -p REPLICA_COUNT=$ReplicaCount -p KEYCLOAK_ENABLED=$KeycloakEnabled
 
     const phases = {};
     // Pivot configuration table, so that `phase name` becomes a top-level property
--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -7,6 +7,7 @@ const MyDeployer = class extends BasicDeployer{
     const settings = this.settings
     const phase = settings.phase
     const phases = settings.phases
+    const config = phases[phase];
     const objects = [];
     const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'));
 
@@ -15,23 +16,30 @@ const MyDeployer = class extends BasicDeployer{
     // TODO: Need to evaluate ImageChangeTrigger from tools?
     objects.push(... oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/fom-db-deploy.yml`, {
       'param':{
-        'SUFFIX': phases[phase].suffix,
+        'SUFFIX': config.suffix,
         // TODO: Add more parameters for prod configuration (storage size, etc.)
       }
     }));
+
+    // TODO: Have an error...
+    // Error: command: oc --namespace=a4b31c-dev apply -f - --output=name
+    // stderr:The Route "fom-api-dev-86" is invalid: spec.host: Invalid value: "fom-nrs-dev-86": field is immutable    
     objects.push(... oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/fom-api-deploy.yml`, {
       'param':{
-        'SUFFIX': phases[phase].suffix,
-        'IMAGE_STREAM_VERSION': phases[phase].tag,
-        'HOSTNAME': `${phases[phase].hostname}`,
+        'SUFFIX': config.suffix,
+        'IMAGE_STREAM_VERSION': config.tag,
+        'HOSTNAME': `${config.hostname}`,
+        'DB_TESTDATA': `${config.testDataEnabled}`,
+        'REPLICA_COUNT': `${config.apiReplicaCount}`,
+        'KEYCLOAK_ENABLED': `${config.keycloakEnabled}`,
         // TODO: Add more parameters...
       }
     }));
     // TODO: Need to change to pull from imagestream in same namespace, not from tools. 
     objects.push(... oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/fom-batch-deploy.yml`, {
       'param':{
-        'SUFFIX': phases[phase].suffix,
-        'IMAGE_STREAM_VERSION': phases[phase].tag,
+        'SUFFIX': config.suffix,
+        'IMAGE_STREAM_VERSION': config.tag,
       }
     }));
 
