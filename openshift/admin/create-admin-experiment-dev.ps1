05b4540fb96d035adb2da5966998a6ea78d0f152
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-07-21 17:02:00 -0600

    Openshift cleanup

--- openshift/admin/create-admin-experiment-dev.ps1
+++
@@ -1,5 +0,0 @@
-. ./create-admin-function.ps1
-
-DeleteAdminFrontEnd -Suffix "-experiment-dev" -Env "dev"
-
-#CreateAdminFrontEnd -Suffix "-experiment-dev" -Env "dev" -ApiVersion "dev" -ReplicaCount 1
--- openshift/public/create-public-experiment-dev.ps1
+++
@@ -1,5 +0,0 @@
-. ./create-public-function.ps1
-
-DeletePublicFrontEnd -Suffix "-experiment-dev" -Env "dev"
-
-#CreatePublicFrontEnd -Suffix "-experiment-dev" -Env "dev" -ApiVersion "dev" -ReplicaCount 1
