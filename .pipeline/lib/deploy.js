f65e56a0ed2660120c836dac1fef5fdeaa3c6e93
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-07-22 19:04:45 -0600

    Add configuration for object storage in openshift.

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -32,6 +32,7 @@ const MyDeployer = class extends BasicDeployer{
     objects.push(... oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/api/fom-api-deploy.yml`, {
       'param':{
         ...appParams,
+        'OBJECT_STORAGE_URL': config.objectStorageUrl,
         'KEYCLOAK_URL': config.keycloakUrl,
         'REPLICA_COUNT': config.apiReplicaCount,
         // TODO: Add more parameters for prod configuration (CPU, memory, etc.)
