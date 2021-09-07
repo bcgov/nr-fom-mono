3eb06cad927ec14c5ca9864cc829142d33fa1787
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-09-07 12:41:18 -0600

    Confirmed fix to extension creation.

--- docker/db/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ docker/db/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -8,17 +8,17 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   echo "Creating database ${APP_DATABASE}"
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
-  echo "value of 1 = $1"
   echo "Creating extensions..."
-  # Get error: psql: error: FATAL:  role "dbname=postgres user=postgres host=localhost port=5432" does not exist
+  # Value of $1 =  dbname=postgres user=postgres host=localhost port=5432. We're changing the database so don't use the $1.
+  
   # PostGIS extensions
   psql -U postgres -w -d ${APP_DATABASE} -c "create extension if not exists postgis;"
-  #psql "$1" -w -d ${APP_DATABASE} -c "create extension if not exists postgis_topology;"
-  #psql "$1" -w -d ${APP_DATABASE} -c "create extension if not exists fuzzystrmatch;"
-  #psql "$1" -w -d ${APP_DATABASE} -c "create extension if not exists postgis_tiger_geocoder;"
+  psql -U postgres -w -d ${APP_DATABASE} -c "create extension if not exists postgis_topology;"
+  psql -U postgres -w -d ${APP_DATABASE} -c "create extension if not exists fuzzystrmatch;"
+  psql -U postgres -w -d ${APP_DATABASE} -c "create extension if not exists postgis_tiger_geocoder;"
 
   # FOM specific extensions
-  #psql "$1" -w -d ${APP_DATABASE} -c "create extension if not exists pgcrypto;"
+  psql -U postgres -w -d ${APP_DATABASE} -c "create extension if not exists pgcrypto;"
 
 else
   echo "Skipping user creation"
