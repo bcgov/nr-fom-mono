5021f9271a7e14775ac044d15bb593cf10ac9480
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-02-07 12:53:29 -0700

    FOM-103 Fix for lack of super-user permission.

--- apps/api/src/migrations/main/1643935525393-forestClientNumber8Characters.js
+++ apps/api/src/migrations/main/1643935525393-forestClientNumber8Characters.js
@@ -6,8 +6,10 @@ module.exports = class forestClientNumber8Characters1643935525393 {
         console.log('Starting forest client number 8 character migration');
 
         await queryRunner.query(`
-        -- Disable foreign key constraints (implemented as triggers)
-        alter table app_fom.project disable trigger all;
+        -- Disable foreign key constraints (implemented as triggers). 
+        -- In OpenShift, don't have permissions (not superuser) to do this, so use workaround of changing session replication role setting.
+        -- alter table app_fom.project disable trigger all;
+        set session_replication_role to replica;
         
         update app_fom.project set forest_client_number = lpad(forest_client_number, 8, '0');
         
@@ -16,7 +18,8 @@ module.exports = class forestClientNumber8Characters1643935525393 {
         
         update app_fom.forest_client set forest_client_number = lpad(forest_client_number, 8, '0');
         
-        alter table app_fom.project enable trigger all;
+        -- alter table app_fom.project enable trigger all;
+        set session_replication_role to default;
         `);
         
     }
