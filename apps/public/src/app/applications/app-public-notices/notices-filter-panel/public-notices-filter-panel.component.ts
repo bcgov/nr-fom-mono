d4b220864a96973c014ece8c5417fc736a4f31c0
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2022-04-09 20:25:42 -0700

    FOM-111 - Add filter fields. - Use @input and @output for communication.

--- apps/public/src/app/applications/app-public-notices/notices-filter-panel/public-notices-filter-panel.component.ts
+++ apps/public/src/app/applications/app-public-notices/notices-filter-panel/public-notices-filter-panel.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import moment = require('moment');
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { IFilterFields } from '../../utils/filter';
+import moment = require('moment');
 
 @Component({
   selector: 'notices-filter-panel',
@@ -9,10 +9,29 @@ import { IFilterFields } from '../../utils/filter';
 })
 export class PublicNoticesFilterPanelComponent implements OnDestroy, OnInit {
 
+  filter: NoticeFilter;
+  maxDate: Date = new Date();
+
+  @Input() 
+  districtList: string[];
+  
+  @Output() 
+  filterPublicNoticesEvt = new EventEmitter<NoticeFilter>();
+
   ngOnInit(): void {
+    this.filter = new NoticeFilter();
+  }
+
+  onFilterChange(): void {
+    this.filterPublicNoticesEvt.emit(this.filter);
   }
 
   ngOnDestroy() {
   }
+}
+export class NoticeFilter {
+  forestClientName: IFilterFields<string> = { queryParam: 'forestClientName', value: null};
+  districtName: IFilterFields<string> = { queryParam: 'districtName', value: null};
+  commentingOpenDate: IFilterFields<Date> = { queryParam: 'commentingOpenDate', value: null};
 }
 
--- apps/public/src/app/applications/app-public-notices/public-notices-panel.component.ts
+++ apps/public/src/app/applications/app-public-notices/public-notices-panel.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import {MatAccordion} from '@angular/material/expansion';
-import { PublicNoticeService } from '@api-client';
+import { MatAccordion } from '@angular/material/expansion';
+import { PublicNoticePublicFrontEndResponse, PublicNoticeService } from '@api-client';
 import { UrlService } from '@public-core/services/url.service';
-import * as _ from 'lodash';
-import moment = require('moment');
 import { IUpdateEvent } from '../projects.component';
 import { Panel } from '../utils/panel.enum';
+import { NoticeFilter } from './notices-filter-panel/public-notices-filter-panel.component';
+import moment = require('moment');
 
 @Component({
   selector: 'app-public-notices-panel',
@@ -17,7 +17,8 @@ export class PublicNoticesPanelComponent implements OnDestroy, OnInit {
   @ViewChild(MatAccordion) accordion: MatAccordion;
   
   isLoading = false;
-  pNotices: any[];
+  pNotices: Array<PublicNoticePublicFrontEndResponse>;
+  districtList: string[]
 
   constructor(
     public urlService: UrlService,
@@ -28,9 +29,15 @@ export class PublicNoticesPanelComponent implements OnDestroy, OnInit {
     this.publicNoticeService
       .publicNoticeControllerFindListForPublicFrontEnd()
       .subscribe((results) => {
-        this.pNotices = results
+        this.pNotices = results;
+        if (this.pNotices) {
+          this.districtList = [...new Set(
+              this.pNotices
+                .filter(pn => pn.project.district != undefined)
+                .map(pn => pn.project.district?.name)
+            )].sort();
+        }
       });
-      // .subscribe(results => this.pNotices = this.getMockPublicNotices()); TODO: remove this hardcode mock data later.
   }
 
   public showDetails(id: number) {
@@ -41,72 +48,11 @@ export class PublicNoticesPanelComponent implements OnDestroy, OnInit {
     }, 450);
   }
 
-  ngOnDestroy() {
+  public handlePublicNoticesFilterUpdate(updateEvent: NoticeFilter) {
+    // TODO...
   }
 
-  private getMockPublicNotices() {
-    return [
-      {
-        "projectId": 3,
-        "reviewAddress": "Suite #123, 456 Some Very Long Name Street, Vancouver BC",
-        "reviewBusinessHours": "Monday to Friday 8am to 5pm, Weekends 10am - 3pm",
-        "receiveCommentsAddress": null,
-        "receiveCommentsBusinessHours": null,
-        "isReceiveCommentsSameAsReview": true,
-        "mailingAddress": "P.O. Box 12345678 Surrey BC",
-        "email": "info@industrydomain.com",
-        "project": {
-          "commentingClosedDate": "2022-04-01",
-          "commentingOpenDate": "2021-04-01",
-          "validityEndDate": "2024-04-01",
-          "createTimestamp": "2021-09-08T19:56:07.713Z",
-          "description": "Commenting open with submission project. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. ",
-          "district": {
-            "id": 43,
-            "name": "Campbell River"
-          },
-          "forestClient": {
-            "id": "00001012",
-            "name": "BELL LUMBER & POLE CANADA, ULC"
-          },
-          "fspId": 10,
-          "id": 3,
-          "name": "Fake name 3 with lots of comments",
-          "revisionCount": 19,
-          "commentClassificationMandatory": false
-        }
-      },
-       {
-        "projectId": 2440,
-        "reviewAddress": "Suite #123, 456 Some Very Long Name Street, Vancouver BC",
-        "reviewBusinessHours": "Monday to Friday 8am to 5pm, Weekends 10am - 3pm",
-        "receiveCommentsAddress": "101-11234 West 12th Avenue, Vancouver BC V6H 1L9",
-        "receiveCommentsBusinessHours": "Monday to Friday 8am to 5pm, Saturday 10am - 2pm",
-        "isReceiveCommentsSameAsReview": false,
-        "mailingAddress": "P.O. Box 12345678 Surrey BC",
-        "email": "info@industrydomain.com",
-        "project": {
-          "commentingClosedDate": "2022-04-01",
-          "commentingOpenDate": "2021-04-01",
-          "validityEndDate": "2024-04-01",
-          "createTimestamp": "2021-09-08T19:56:07.713Z",
-          "description": "Commenting open with submission project. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. ",
-          "district": {
-            "id": 43,
-            "name": "Campbell River"
-          },
-          "forestClient": {
-            "id": "00001012",
-            "name": "BELL LUMBER & POLE CANADA, ULC"
-          },
-          "fspId": 10,
-          "id": 3,
-          "name": "Fake name 3 with lots of comments",
-          "revisionCount": 19,
-          "commentClassificationMandatory": false
-        }
-      }
-    ];
+  ngOnDestroy() {
   }
 
 }
