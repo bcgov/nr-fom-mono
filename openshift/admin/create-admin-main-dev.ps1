0a0bd5a60922f5e3097c64966e9976e7a3a9b4ae
Derek Roberts <derek.roberts@gmail.com>
2022-06-13 18:18:41 -0700

    Repo cleanup after FOM merge (#8)

--- openshift/admin/create-admin-main-dev.ps1
+++
@@ -1,5 +0,0 @@
-. ./create-admin-function.ps1
-
-#DeleteAdminFrontEnd -Suffix "-main-dev" -Env "dev"
-
-CreateAdminFrontEnd -Suffix "-main-dev" -Env "dev" -ApiVersion "main" 
--- openshift/clean-up-dev-stack.ps1
+++
@@ -1,6 +0,0 @@
-# Remove an old dev stack. (Each new PR to master creates a brand new dev stack)
-# Don't delete the N-1 stack just in case for production support.
-oc project a4b31c-dev
-oc delete all,NetworkPolicy,ConfigMap,Secret,PersistentVolumeClaim -l app=fom-dev-86
-
-
