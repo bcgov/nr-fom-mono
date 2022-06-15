479b88454ac46897507a3a81b04e5e179f81a296
Derek Roberts <derek.roberts@gmail.com>
2022-06-15 08:57:26 -0700

    Adjust migration paths (#15)

--- apps/api/src/migrations/ormconfig-migration-test.ts
+++ apps/api/src/migrations/ormconfig-migration-test.ts
@@ -2,20 +2,22 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import * as config from "../ormconfig";
 
 var ormConfig = { 
-    ...config, 
-    schema: '', // Use default (public) schema for migration table to avoid bootstrapping error where the app_fom schema doesn't exist yet to check the migration table.
-    migrations: [
-      // Docker-installed migration files
-      '/app/apps/api/migrations/test/*.js',
-      // Local post-build migration files
-      './migrations/test/*.js',
-      // Source migration files used in development    
-      './apps/api/src/migrations/test/*{.ts,.js}'
-    ],
-    migrationsTableName: 'migration_test', 
-    cli: {
-        'migrationsDir': './apps/api/src/migrations/test'
-      }      
+  ...config, 
+  schema: '', // Use default (public) schema for migration table to avoid bootstrapping error where the app_fom schema doesn't exist yet to check the migration table.
+  migrations: [
+    // OpenShift
+    '/app/dist/apps/api/migrations/test/*.js',
+    // Docker
+    '/app/apps/api/migrations/test/*.js',
+    // Local post-build migration files
+    './migrations/test/*.js',
+    // Source migration files used in development    
+    './apps/api/src/migrations/test/*{.ts,.js}'
+  ],
+  migrationsTableName: 'migration_test', 
+  cli: {
+      'migrationsDir': './apps/api/src/migrations/test'
+    }      
 } as TypeOrmModuleOptions;
 
 module.exports = ormConfig;
