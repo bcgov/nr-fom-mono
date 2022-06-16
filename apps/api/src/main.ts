6db42586a7fc972ee2aacde472fbf4419b1d80c8
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2022-06-16 15:42:06 -0700

    Remove previous logging, BYPASS_CORS is working.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -51,14 +51,13 @@ async function bootstrap():Promise<INestApplication> {
   app.use(json({ limit: MAX_CONTENT_LIMIT }));
   app.use(urlencoded({ extended: true, limit: MAX_CONTENT_LIMIT }));
 
-  console.log("Env 'BYPASS_CORS' value: ", process.env.BYPASS_CORS); // TODO: remove this, temporary logging check.
   if (process.env.BYPASS_CORS === "true") {
-    console.log("BYPASS CORS: true"); // TODO: remove this, temporary logging check.
-    // For local development only, leave env var undefined within OpenShift deployments.
+    // For local development only; should set env var to 'false' within OpenShift deployments.
     app.enableCors({
       origin: '*',
       credentials: false,
     });
+    console.log("CORS bypassed.");
   }
 
   const httpAdapter = app.getHttpAdapter().getInstance();
