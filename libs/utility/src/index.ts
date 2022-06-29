f2e200205c91ec723f3271f1727a216f0ac4902f
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2022-06-29 12:30:03 -0700

    Remove previosly duplicated typescript class 'export' and add one new export './models/map-layers'.

--- libs/utility/src/index.ts
+++ libs/utility/src/index.ts
@@ -2,9 +2,5 @@ export * from './utility.module';
 export * from './utility.service';
 
 export * from './models/primitive-keys.model';
-
-export * from './models/primitive-keys.model';
-export * from './models/primitive-keys.model';
-export * from './models/primitive-keys.model';
-export * from './models/primitive-keys.model';
+export * from './models/map-layers';
 export * from './types/type-names.type';
