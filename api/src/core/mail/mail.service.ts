de256ecbccca93c627f6b718dcc278d102f1f421
Basil Vandegriend <basil.vandegriend@cgi.com>
2022-08-03 14:48:24 -0600

    Add demo environment target email.

--- api/src/core/mail/mail.service.ts
+++ api/src/core/mail/mail.service.ts
@@ -11,12 +11,14 @@ export class MailService {
     const env = process.env.ENV || 'dev';
     const isProd = (env == 'prod');
     const isTest = (env == 'test');
+    const isDemo = (env == 'demo');
     const districtEmail = project.district.email + "@gov.bc.ca"; // production - table stores emails without the @gov.bc.ca suffix.
-    const testEnvTo = 'FLNR.AdminServicesCariboo@gov.bc.ca'; // test
+    const testEnvTo = 'FLNR.AdminServicesCariboo@gov.bc.ca'; 
+    const demoEnvTo = 'FLNR.AdminServicesCariboo@gov.bc.ca'; 
     const host = process.env.HOSTNAME? `https://${process.env.HOSTNAME}`: 'http://localhost:4200';
     const fomViewLink = `${host}/admin/a/${project.id}`;
 
-    const to = isProd? districtEmail: (isTest? testEnvTo: 'basil.vandegriend@cgi.com');
+    const to = isProd? districtEmail: (isTest? testEnvTo: (isDemo? demoEnvTo : 'basil.vandegriend@cgi.com'));
     const from = '"FOMDoNotReply" <Do-Not-Reply@gov.bc.ca'; // override default from;
     const prodRecipientMsg = isProd? '' : ` Production recipient = ${districtEmail}`;
     this.logger.info(`Sending FOM ${project.id} finalized notification email to ${to}${prodRecipientMsg}`);
