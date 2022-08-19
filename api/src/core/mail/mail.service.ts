8c0a8c61dc16a367df22e7a6a046084cbeab25d2
Derek Roberts <derek.roberts@gmail.com>
2022-08-19 09:04:52 -0700

    Update emails for finalized FOMs (#225)

--- api/src/core/mail/mail.service.ts
+++ api/src/core/mail/mail.service.ts
@@ -8,22 +8,26 @@ export class MailService {
   constructor(private mailerService: MailerService, private logger: PinoLogger) {}
 
   async sendDistrictNotification(project: Project) {
-    const env = process.env.ENV || 'dev';
-    const isProd = (env == 'prod');
-    const isTest = (env == 'test');
+
+    // Email address used for PROD, populated from the Db
     const districtEmail = project.district.email + "@gov.bc.ca"; // production - table stores emails without the @gov.bc.ca suffix.
-    const testEnvTo = 'FLNR.AdminServicesCariboo@gov.bc.ca'; // test
+
+    // Use ${districtEmail} in PROD, otherwise provided in var for lower environments (DEV, TEST, DEMO)
+    const to = process.env.FOM_EMAIL_NOTIFY ? `${process.env.FOM_EMAIL_NOTIFY}` : `${districtEmail}`
+
+    // Link to FOM
     const host = process.env.HOSTNAME? `https://${process.env.HOSTNAME}`: 'http://localhost:4200';
     const fomViewLink = `${host}/admin/a/${project.id}`;
 
-    const to = isProd? districtEmail: (isTest? testEnvTo: 'basil.vandegriend@cgi.com');
+    // From email address
     const from = '"FOMDoNotReply" <Do-Not-Reply@gov.bc.ca'; // override default from;
-    const prodRecipientMsg = isProd? '' : ` Production recipient = ${districtEmail}`;
-    this.logger.info(`Sending FOM ${project.id} finalized notification email to ${to}${prodRecipientMsg}`);
+
+    // Log and send email
+    this.logger.info(`Sending FOM ${project.id} finalized notification email to ${to}`);
     await this.mailerService.sendMail({
       to: to,
       from: from,
-      subject: `<${env}> New Final FOM submission received`,
+      subject: `New Final FOM submission received for ${host}/admin/`,
       html: `<h3>FOM ${project.id} ${project.name} ${project.forestClient.name} has been finalized and is available for review.</h3>
         <p>
           Please use this <a href="${fomViewLink}" target="_blank">link</a> to access FOM details.
