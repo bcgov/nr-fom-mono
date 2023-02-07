067a0b3e096c888eba20641eca49d7783e1f5958
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2023-02-07 12:53:38 -0800

    Add new AwsCognitoConfig class and use new 'AUTH_PROVIDER' environment variable to switch keycloak and aws

--- api/src/core/security/auth.service.ts
+++ api/src/core/security/auth.service.ts
@@ -5,7 +5,7 @@ import { JwksClient } from 'jwks-rsa';
 import { PinoLogger } from 'nestjs-pino';
 
 import { User } from '@utility/security/user';
-
+import * as aswCognitoEnvJson from '../../../aws-cognito-env.json';
 
 // Both of these decorators requires the global AuthInterceptor to add the User object to the request. If no bearer token is provided, the user object will be null.
 /**
@@ -29,58 +29,122 @@ export const UserRequiredHeader = createParamDecorator( (data: unknown, ctx: Exe
 });
 
 export class KeycloakConfig {
-    @ApiProperty()
-    enabled: boolean = true;
+  @ApiProperty()
+  enabled: boolean = true;
 
-    // Keycloak Server URL
-    @ApiProperty()
-    url: string;
+  // Keycloak Server URL
+  @ApiProperty()
+  url: string;
 
-    @ApiProperty()
-    siteMinderUrl: string;
+  @ApiProperty()
+  siteMinderUrl: string;
 
-    @ApiProperty()
-    realm: string;
+  @ApiProperty()
+  realm: string;
 
-    @ApiProperty()
-    clientId: string = 'fom';
+  @ApiProperty()
+  clientId: string = 'fom';
 
-    getIssuer(): string {
-      return this.url + "/realms/" + this.realm;
-    }
-
-    getCertsUri(): string {
-      return this.getIssuer() + "/protocol/openid-connect/certs";
-    }
+  getIssuer(): string {
+    return this.url + "/realms/" + this.realm;
   }
+
+  getCertsUri(): string {
+    return this.getIssuer() + "/protocol/openid-connect/certs";
+  }
+}
+
+export class AwsCognitoConfig {
+  @ApiProperty()
+  enabled: boolean = true;
+
+  @ApiProperty()
+  region: string;
+
+  @ApiProperty()
+  userPoolsId: string;
+
+  @ApiProperty()
+  userPoolWebClientId: string;
+
+  @ApiProperty()
+  mandatorySignIn: boolean = true;
+
+  @ApiProperty()
+  federationTarget: string = 'COGNITO_USER_POOLS'
+
+  @ApiProperty()
+  domain: string; // 'your_cognito_domain'
+
+  @ApiProperty()
+  scope: string[] = ['openid'];
+
+  @ApiProperty()
+  signUpVerificationMethod: string = 'code'
+
+  @ApiProperty()
+  frontendRedirectBaseUrl: string; // Frontend needs. 
+
+  /* Can't find out where to get getCertsUri from aws. Maybe it only needs to know issuer...
+  // probably don't need this in amplify
+  getIssuer(): string {
+    // fam api setup: issuer=f"https://cognito-idp.{aws_region}.amazonaws.com/{user_pool_id}"
+    return `https://cognito-idp.${this.region}.amazonaws.com/${this.userPoolsId}`
+  }
+
+  // probably don't need this in amplify
+  getCertsUri(): string {
+    // return this.getIssuer() + "/protocol/openid-connect/certs";
+    return this.getIssuer() + ""
+  }
+  */
+}
+
+export enum AUTH_PROVIDER {
+    KEYCLOAK = 'KEYCLOAK',
+    AWS_COGNITO = 'AWS_COGNITO'
+}
 
 @Injectable()
 export class AuthService {
+    private providedAuth: string;
     private config:KeycloakConfig = new KeycloakConfig();
+    private awsConfig: AwsCognitoConfig;
 
     private jwksClient;
 
     constructor(private logger: PinoLogger) {
-        // Defaults are for local development. Keycloak enabled by default for maximum security.
-        this.config.enabled = (process.env.KEYCLOAK_ENABLED || 'true') === 'true';
-        this.config.realm = process.env.KEYCLOAK_REALM || 'ichqx89w';
-        this.config.url = process.env.KEYCLOAK_URL || 'https://dev.oidc.gov.bc.ca/auth';
-        this.config.siteMinderUrl = process.env.SITEMINDER_URL || 'https://logontest7.gov.bc.ca';
-        // Sample User = {"isMinistry":true,"isForestClient":true,"clientIds":[1011, 1012],"userName":"fakeuser@idir","displayName":"Longlastname, Firstname"}
-        // Other values for Keycloak URL: TEST: https://test.oidc.gov.bc.ca/auth, PROD: https://oidc.gov.bc.ca/auth
+        this.providedAuth = (process.env.AUTH_PROVIDER || AUTH_PROVIDER.KEYCLOAK); // Default to 'KEYCLOAK' if not provided.
+        // TODO: this part when it is working for aws, can refactor or retire keycloak.
+        if (AUTH_PROVIDER.KEYCLOAK === this.providedAuth) {
+            // Defaults are for local development. Keycloak enabled by default for maximum security.
+            this.config.enabled = (process.env.KEYCLOAK_ENABLED || 'true') === 'true';
+            this.config.realm = process.env.KEYCLOAK_REALM || 'ichqx89w';
+            this.config.url = process.env.KEYCLOAK_URL || 'https://dev.oidc.gov.bc.ca/auth';
+            this.config.siteMinderUrl = process.env.SITEMINDER_URL || 'https://logontest7.gov.bc.ca';
+            // Sample User = {"isMinistry":true,"isForestClient":true,"clientIds":[1011, 1012],"userName":"fakeuser@idir","displayName":"Longlastname, Firstname"}
+            // Other values for Keycloak URL: TEST: https://test.oidc.gov.bc.ca/auth, PROD: https://oidc.gov.bc.ca/auth
 
-        this.logger.info("Keycloak configuration %o", this.config);
+            this.logger.info("Keycloak configuration %o", this.config);
 
-        this.jwksClient = new JwksClient({
-          jwksUri: this.config.getCertsUri(),
-          cache: true, // Accept cache defaults
-          rateLimit: true,
-        });
-
+            this.jwksClient = new JwksClient({
+                jwksUri: this.config.getCertsUri(),
+                cache: true, // Accept cache defaults
+                rateLimit: true,
+            });
+        }
+        else {
+            this.awsConfig = Object.assign(new AwsCognitoConfig(), aswCognitoEnvJson);
+            console.log(this.awsConfig)
+        }
     }
 
-    getKeycloakConfig():KeycloakConfig {
+    getKeycloakConfig(): KeycloakConfig {
         return this.config;
+    }
+
+    getAwsCognitoConfig(): AwsCognitoConfig {
+       return this.awsConfig;
     }
 
     async verifyToken(authHeader: string):Promise<User> {
