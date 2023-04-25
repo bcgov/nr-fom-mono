70ee75072c2d1353aa78f720ed9dfa2de44abfc7
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2023-04-25 15:59:07 -0700

    Fix jest.config.js import ts-jest issue and remove jest configuration from package.json as it now does not support two configuration exists at workspace.

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,4 +1,4 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
@@ -16,6 +16,16 @@ module.exports = {
   // 'moduleNameMapper' important to map some 'paths' from tsconfig.json for jest.
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' } ),
   moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.(t|j)s"
+  ],
   coverageDirectory: '../coverage/api',
-  "testEnvironment": "node"
+  coverageReporters: [
+    "text",
+    "lcov",
+    "cobertura"
+  ],
+  testResultsProcessor: "jest-sonar-reporter",
+  testEnvironment: "node"
 };
