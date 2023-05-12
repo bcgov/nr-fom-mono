7c2d7653b0b57d65f20a77ca6c0f4007cd7efa32
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2023-05-12 12:53:21 -0700

    Public: replace AppRoutingModule and have public side @NgModule free.

--- public/src/main.ts
+++ public/src/main.ts
@@ -3,14 +3,15 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { ApiModule, Configuration } from '@api-client';
 import { ErrorInterceptor } from '@public-core/interceptors/http-error.interceptor';
 import { retrieveApiBasePath } from '@utility/services/config.service';
-import { AppRoutingModule } from 'app/app-routing.module';
 import { AppComponent } from 'app/app.component';
+import { AppRoutes } from 'app/app.routes';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { environment } from './environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
@@ -19,20 +20,28 @@ const apiConfig = new Configuration({
     basePath: retrieveApiBasePath()
 });
 
+const coreProviders = [
+    provideHttpClient(),
+    importProvidersFrom(ApiModule.forRoot(() => apiConfig)),
+    importProvidersFrom(BsDatepickerModule.forRoot()),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(MatDialogModule),
+    importProvidersFrom(MatSnackBarModule),
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: ErrorInterceptor,
+        multi: true
+    },
+]
+
+const routesProviders = [
+    provideRouter(AppRoutes)
+]
+
 // Bootstrap standalone root AppComponent.
 bootstrapApplication(AppComponent, {
     providers: [
-        provideHttpClient(),
-        importProvidersFrom(ApiModule.forRoot(() => apiConfig)),
-        importProvidersFrom(BsDatepickerModule.forRoot()),
-        importProvidersFrom(AppRoutingModule),
-        importProvidersFrom(BrowserAnimationsModule),
-        importProvidersFrom(MatDialogModule),
-        importProvidersFrom(MatSnackBarModule),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: ErrorInterceptor,
-            multi: true
-        },
+        ...coreProviders,
+        ...routesProviders
     ]
 })
