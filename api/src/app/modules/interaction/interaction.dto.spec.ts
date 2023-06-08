dfacbe7e850201c43502d17769ca59578b183329
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2023-06-08 09:40:38 -0700

    Add one more test

--- api/src/app/modules/interaction/interaction.dto.spec.ts
+++ api/src/app/modules/interaction/interaction.dto.spec.ts
@@ -4,12 +4,17 @@ import { isValidDateOnlyString } from "@api-modules/interaction/interaction.dto"
 describe('interaction.dto', () => {
     describe('isValidDateOnlyString', () => {
         const validTestDateStringValue = "2023-06-07";
+        const wrongPatternDateStringValue = "06-07-2023";
         const wrongStringArgument = "some-wrong-string";
         const invalidFormatDateStringValue = "2003-06-07 03:30:45"; // should not have time.
         const jsDateStringIsInvalid = "Sat Jun 03 2023 00:00:00 GMT-0700 (Pacific Daylight Time)"; // direct js Date() string should not match.
 
         it (`Date string "${validTestDateStringValue}" is valid ${DateTimeUtil.DATE_FORMAT} string`, async () => {
             expect(isValidDateOnlyString(validTestDateStringValue)).toBe(true);
+        });
+
+        it (`Date string "${wrongPatternDateStringValue}" pattern not match ${DateTimeUtil.DATE_FORMAT}`, async () => {
+            expect(isValidDateOnlyString(wrongPatternDateStringValue)).toBe(false);
         });
 
         it (`Date string ${wrongStringArgument} is invalid`, async () => {
