25155b29b21f1b99396cf1fa1b21ee9cca431a33
Ian Liu <ianliuwk1019@gmail.com>
2023-06-29 22:33:08 -0700

    Add @elastic/ecs-pino-format, and make setup works.

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,62 +1,47 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Core Modules
 import { AttachmentModule } from './modules/attachment/attachment.module';
-import { InteractionModule } from './modules/interaction/interaction.module';
 import { DistrictModule } from './modules/district/district.module';
 import { ForestClientModule } from './modules/forest-client/forest-client.module';
-import { ProjectModule } from './modules/project/project.module';
+import { InteractionModule } from './modules/interaction/interaction.module';
 import { ProjectAuthModule } from './modules/project/project-auth.module';
+import { ProjectModule } from './modules/project/project.module';
 import { PublicCommentModule } from './modules/public-comment/public-comment.module';
-import { SubmissionModule } from './modules/submission/submission.module';
 import { SpatialFeatureModule } from './modules/spatial-feature/spatial-feature.module';
+import { SubmissionModule } from './modules/submission/submission.module';
 
 // Other Modules
 import { LoggerModule, Params } from 'nestjs-pino';
+import pino from 'pino';
+import { SecurityModule } from '../core/security/security.module';
 import { AppConfigModule } from './modules/app-config/app-config.module';
 import { AppConfigService } from './modules/app-config/app-config.provider';
-import { SecurityModule } from '../core/security/security.module'
+import ecsFormat = require('@elastic/ecs-pino-format')
 
 function getLogLevel():string {
   return process.env.LOG_LEVEL || 'info';
 }
 
-const logParams: Params = { 
-    pinoHttp: {
-        // level: getLogLevel(),
-        // timestamp: () => { 
-        //     const time = new Date().toISOString();
-        //     return `,"time": ${time}`;
-        // },
-        // formatters: {
-        //     level: (label) => { return { level: label }; }
-        // },
+const ecsOptions = ecsFormat();
+const streams = [
+  { stream: pino.destination(1) }, // terminal stdout.
+  { stream: pino.destination({ dest: './logs/app.log' }) }
+]
 
-        transport: {
-            targets: [
-                {
-                    target: 'pino/file',
-                    options: {
-                        destination: process.env.LOG_PATH || './logs/app.log',
-                        mkdir: true,
-                        // timestamp: stdTimeFunctions.isoTime // does not work
-                    },
-                    level: getLogLevel()
-                },
-                {
-                    target: 'pino-pretty',
-                    options: {
-                        translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'",
-                        colorize: false,
-                        singleLine: true
-                    },
-                    level: getLogLevel()
-                },
-            ]
-        }
-    }
+const logParams: Params = { 
+  pinoHttp: [{
+    ...ecsOptions, // default options
+    customAttributeKeys: { // some other ecs format using custom override.
+        req: 'http.request',
+        res: 'http.response',
+        responseTime: 'event.duration',
+    },
+    level: getLogLevel(),
+  },
+  pino.multistream(streams)] // multistream so can have stdout & file
 }
 
 @Module({
@@ -98,3 +83,49 @@ const logParams: Params = {
 export class AppModule {
   constructor(private appConfigService: AppConfigService) {}
 }
+
+/*
+// Using transport with target(pion/file, pino-pretty), not ecs format.
+{
+  pinoHttp: {
+    transport: {
+      targets: [
+          {
+              target: 'pino/file',
+              options: {
+                  destination: process.env.LOG_PATH || './logs/app.log',
+                  mkdir: true,
+                  // timestamp: stdTimeFunctions.isoTime // does not work
+              },
+              level: getLogLevel()
+          },
+          {
+              target: 'pino-pretty',
+              options: {
+                  translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'",
+                  colorize: false,
+                  singleLine: true
+              },
+              level: getLogLevel()
+          },
+      ]
+    }
+  }
+}
+*/
+
+/*
+  // originnal pino setup.
+  {
+    pinoHttp: {
+      level: getLogLevel(),
+      timestamp: () => { 
+          const time = new Date().toISOString();
+          return `,"time": ${time}`;
+      },
+      formatters: {
+          level: (label) => { return { level: label }; }
+      },
+    }
+  }
+*/
