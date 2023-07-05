51f94c07c9bbbb072081d591b8f262ed383acb94
Ian Liu <ianliuwk1019@gmail.com>
2023-07-05 13:43:16 -0700

    Fix comment where it should belong to.

--- api/src/app/modules/interaction/interaction.dto.ts
+++ api/src/app/modules/interaction/interaction.dto.ts
@@ -22,18 +22,6 @@ export function IsISODateOnlyString(validationOptions?: ValidationOptions) {
     }    
 }
 
-// /*
-// Limited to only check date string value with backend defined date format: DateTimeUtil.DATE_FORMAT.
-// Valid date string like "2023-06-07" has length: 10 (so, no time portion).
-// Note: "2023-06-07" and "06-07-2023" and "2023/06/07" etc are treated valid from dayjs library and all
-//     are translated into "2023-06-07" by the dayjs library.
-// For now it does not provide flexbility to check other format.
-// */
-// export function isValidDateOnlyString(value: string): boolean {
-//     dayjs.extend(customParseFormat);
-//     return !_.isEmpty(value) && value.length == 10 && dayjs(value, DateTimeUtil.DATE_FORMAT, true).isValid();
-// }
-
 export class InteractionCreateRequest {
   constructor(projectId = null, 
               stakeholder = null, 
