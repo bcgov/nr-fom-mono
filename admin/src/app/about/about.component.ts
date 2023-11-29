a187d1f02cd39abdc5970f65acb3150fe26addf5
Ian Liu <ianliuwk1019@gmail.com>
2023-11-29 12:06:37 -0800

    Add About page and route

---
+++ admin/src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+  constructor() {
+    // Deliberately empty
+  }
+}
