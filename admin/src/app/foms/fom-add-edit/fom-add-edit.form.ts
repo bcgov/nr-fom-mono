133169ec2825be6dcfb2e8827101642af56dcbc2
Ian Liu <ianliuwk1019@gmail.com>
2024-01-07 11:54:42 -0800

    Add conditional expression for @minDate; esentially, it becomes "equal or greater than" validator.

--- admin/src/app/foms/fom-add-edit/fom-add-edit.form.ts
+++ admin/src/app/foms/fom-add-edit/fom-add-edit.form.ts
@@ -1,5 +1,6 @@
+import { AbstractControl } from '@angular/forms';
 import { DistrictResponse, ForestClientResponse, ProjectResponse, WorkflowStateCode } from '@api-client';
-import { minLength, prop, required, minDate, maxLength } from '@rxweb/reactive-form-validators';
+import { minLength, prop, required, minDate, maxLength, not } from '@rxweb/reactive-form-validators';
 import moment = require("moment");
 import * as R from 'remeda';
 
@@ -57,7 +58,17 @@ export class FomAddEditForm implements Pick<ProjectResponse,
 
   // Special case. It is at form control, but will be convert into request body for 'operationEndYear' (number).
   @required({message: 'Operation End Year is required.'})
-  @minDate({fieldName:'opStartDate', message: 'Must be equal to or later than the Start of Operations'})
+  @minDate({
+    // In this case, do not use (x,y) arrow expression for validator. 
+    // Use 'function(control)' expression, so it can get current field value through "this.".
+    conditionalExpression: function(control: AbstractControl) {
+      // For 'opStartDate' and 'opEndDate', only need "year" from gthe date; but still use "Date" type for datePicker.
+      // (This is a tricky case to set up "conditionalExpression" validator, as date is passed from datePicker)
+      // So, conditionally, if years are the same, no need to validate on @minDate().
+      const sameYear = moment(this.opStartDate).year() == moment(this.opEndDate).year();
+      return !sameYear;
+    },
+    fieldName:'opStartDate', message: 'Must be equal to or later than the Start of Operations'})
   @prop()
   opEndDate: Date;
 
