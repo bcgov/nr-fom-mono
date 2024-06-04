5f55b620bb5c169176b25bd77cfdb72aae3d32fd
Ian Liu <ianliuwk1019@gmail.com>
2024-06-04 15:04:53 -0700

    Minor adjustment.

--- public/src/core/utils/constants/appUtils.ts
+++ public/src/core/utils/constants/appUtils.ts
@@ -34,7 +34,7 @@ export class AppUtils {
 }
 
 export const getCommentingClosingDate = (commentingClosedDate: string) => {
-    // Note: commenting_closingDate (inclusive) = commenting_closedDate (exclusive) - 1
+    // Note: commenting_closingDate (inclusive) = commenting_closedDate (exclusive) - 1 day
     // The value should only be used for display, not to pass to backend.
     const commentingClosingDate = moment(commentingClosedDate).add(-1, 'd');
     return commentingClosingDate.format('YYYY-MM-DD')
