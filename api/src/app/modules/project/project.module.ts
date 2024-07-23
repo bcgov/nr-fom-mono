c1c47ce146552aed8cd9886ca6616f095abed974
Ian Liu <ianliuwk1019@gmail.com>
2024-07-23 16:19:21 -0700

    Add ProjectPlanCode to project module

--- api/src/app/modules/project/project.module.ts
+++ api/src/app/modules/project/project.module.ts
@@ -1,28 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ProjectController } from './project.controller';
 import { Project } from './project.entity';
 import { ProjectService } from './project.service';
-import { ProjectController } from './project.controller';
 
 import { PublicNotice } from './public-notice.entity';
 
+import { MailModule } from '@api-core/mail/mail.module';
+import { SecurityModule } from '@api-core/security/security.module';
+import { AttachmentModule } from '@api-modules/attachment/attachment.module';
+import { ProjectPlanCode } from '@src/app/modules/project/project-plan-code.entity';
 import { DistrictModule } from '../district/district.module';
 import { ForestClientModule } from '../forest-client/forest-client.module';
-import { SecurityModule } from '@api-core/security/security.module';
-import { WorkflowStateCodeController } from './workflow-state-code.controller';
-import { WorkflowStateCodeService } from './workflow-state-code.service';
-import { WorkflowStateCode } from './workflow-state-code.entity';
-import { ProjectAuthService } from './project-auth.service';
-import { PublicNoticeService } from './public-notice.service';
-import { PublicNoticeController } from './public-notice.controller';
-import { AttachmentModule } from '@api-modules/attachment/attachment.module';
 import { PublicCommentModule } from '../public-comment/public-comment.module';
-import { MailModule } from '@api-core/mail/mail.module';
+import { ProjectAuthService } from './project-auth.service';
+import { PublicNoticeController } from './public-notice.controller';
+import { PublicNoticeService } from './public-notice.service';
+import { WorkflowStateCodeController } from './workflow-state-code.controller';
+import { WorkflowStateCode } from './workflow-state-code.entity';
+import { WorkflowStateCodeService } from './workflow-state-code.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Project, WorkflowStateCode, PublicNotice]),
+    TypeOrmModule.forFeature([Project, WorkflowStateCode, PublicNotice, ProjectPlanCode]),
     DistrictModule,
     ForestClientModule,
     SecurityModule,
