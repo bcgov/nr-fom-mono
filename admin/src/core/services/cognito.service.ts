3453003d7a0c3fd7c1c0166f632f8dfbe4a00507
Ian Liu <81595625+ianliuwk1019@users.noreply.github.com>
2025-01-08 14:41:03 -0800

    feat: #736 AWS amplify upgrade (#748)
    * Update typescript to 5.1.6
    
    * temp: Upgrade angular/cli and angular/core to 16
    
    * temp: upgrade angular material
    
    * temp: upgrade @angular-eslint
    
    * temp: upgrade ng-bootstrap and angular-fontawesome
    
    * angular core/cli upgrade to 16 for admin
    
    * upgrade angular material/cdk to 16 at admin; remove commonjs
    
    * Upgrade angular-fontawesome and ng-bootstrap
    
    * Update typescript and zonejs before angular 17 upgrade.
    
    * update angular core/cli to 17
    
    * Update angular common and various packages to 17
    
    * upgrade angular cdk to 17
    
    * upgrade angular material to 17
    
    * upgrade angular-eslint to 17
    
    * Update ng-bootstrap and ngx-bootstrap
    
    * upgrade libs for angular
    
    * upgrade typescript and zone.js before angular upgrade
    
    * upgrade various angular packages to 17
    
    * Remove not exist NgbNavbar
    
    * @angular-eslint upgrade and some styling adjustment.
    
    * Fix matBadge off issue after angular 17 migration.
    
    * Replace moment with luxon for public
    
    * Add 'luxon' before replacing moment. Re-facgtor some constant. Replacing moment from fom-add-edit compoent with luxon.
    
    * Replace moment with luxon for interaction component.
    
    * Replace moment with luxon for public notice edit component.
    
    * Replace moment with luxon for fom-detail component.
    
    * Remove moment from admin package.
    
    * Replace lodash debounce with remeda funnel
    
    * Replace lodash with remeda  for public-notice component.
    
    * Remove lodash from details-panel component
    
    * Replace lodash with remeda
    
    * Replace isNil with isNullish from remeda for public
    
    * upgrade remeda version at admin and replace lodash isNil to isNullish from remeda.
    
    * Replace lodash with remeda
    
    * Replace file-saver with file-saver-es es module lib
    
    * add allowedCommonJsDependencies to ignore common js warning for only on (leaflet, leaflet.markercluster and object-hash).
    
    * Remove ngx-dropzone and its unused compoent.
    
    * Replace file-saver with file-saver-es at admin
    
    * Use allowedCommonJsDependencies to ignore only leaflet.
    
    * Minor upgrade aws-amplify version
    
    * Upgrade aws-amplify to v6 and adjust cognito.service.ts at admin.
    
    * Add comment
    
    * Minor code rearrangement.
    
    * removed unnecessary async.

--- admin/src/core/services/cognito.service.ts
+++ admin/src/core/services/cognito.service.ts
@@ -3,17 +3,28 @@ import { Injectable } from "@angular/core";
 import { AwsCognitoConfig } from "@api-client";
 import { User } from "@utility/security/user";
 import { ConfigService } from "@utility/services/config.service";
-import type { CognitoUserSession } from "amazon-cognito-identity-js";
-import { Amplify, Auth } from "aws-amplify";
-import { Observable } from "rxjs";
+import {
+  CognitoAccessToken,
+  CognitoIdToken,
+  CognitoUserSession
+} from 'amazon-cognito-identity-js';
+import { Amplify, ResourcesConfig } from "aws-amplify";
+import { fetchAuthSession, getCurrentUser, signInWithRedirect, signOut } from "aws-amplify/auth";
+import { lastValueFrom, Observable } from "rxjs";
 import { getFakeUser } from "./mock-user";
+
+export interface CognitoAuthToken { 
+  id_token: { [id: string]: any }  // decoded jwt payload
+  access_token: { [id: string]: any } // decoded jwt payload
+  jwtToken: CognitoUserSession // original Cognito user session (tokens)
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class CognitoService {
   public awsCognitoConfig: AwsCognitoConfig;
-  private cognitoAuthToken: object;
+  private cognitoAuthToken: CognitoAuthToken;
   private loggedOut: string;
   private fakeUser: User;
   public initialized: boolean = false;
@@ -39,7 +50,7 @@ export class CognitoService {
         return null;
       }
       return new Promise<any>((resolve) => {
-        return Auth.currentAuthenticatedUser()
+        return getCurrentUser()
           .then(async () => {
               console.log("Signed in...");
               await this.refreshToken();
@@ -55,59 +66,13 @@ export class CognitoService {
     }
   }
 
-  private async loadRemoteConfig() {
-    let url: string = this.configService.getApiBasePath() + "/api/awsCognitoConfig";
-    this.awsCognitoConfig = await this.http
-        .get(url, { observe: "body", responseType: "json" })
-        .toPromise() as AwsCognitoConfig;
-
-    console.log("Using cognito config = " + JSON.stringify(this.awsCognitoConfig));
-    Amplify.configure(this.awsCognitoConfig);
-  }
-
-  private getParameterByName(name) {
-    const url = window.location.href;
-    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-    const results = regex.exec(url);
-    if (!results) {
-      return null;
-    }
-    if (!results[2]) {
-      return "";
-    }
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
   /**
-   * See OIDC Attribute Mapping mapping reference:
-   *      https://github.com/bcgov/nr-forests-access-management/wiki/OIDC-Attribute-Mapping
-   * The display username is "custom:idp_username" from token.
-   */
-  private parseToken(authToken: CognitoUserSession): any {
-    const decodedIdToken = authToken.getIdToken().decodePayload();
-    const decodedAccessToken = authToken.getAccessToken().decodePayload();
-    return {
-      id_token: decodedIdToken,
-      access_token: decodedAccessToken,
-      jwtToken: authToken
-    };
-  }
-
-  /**
-   * Amplify method currentSession() will automatically refresh the accessToken and idToken
-   * if tokens are "expired" and a valid refreshToken presented.
-   *   // console.log("currentAuthToken: ", currentAuthToken)
-   *   // console.log("ID Token: ", currentAuthToken.getIdToken().getJwtToken())
-   *   // console.log("Access Token: ", currentAuthToken.getAccessToken().getJwtToken())
-   *
    * Automatically logout if unable to get currentSession().
    */
   async refreshToken() {
     try {
-      const currentAuthToken: CognitoUserSession = await Auth.currentSession();
-      console.log("currentAuthToken: ", currentAuthToken);
-
-      this.cognitoAuthToken = this.parseToken(currentAuthToken);
+      const awsCognitoUserSession = await this.refreshAndObtainAwsCognitoUserSession();
+      this.cognitoAuthToken = this.parseToken(awsCognitoUserSession);
     } catch (error) {
       console.error("Problem refreshing token or token is invalidated:", error);
       // logout and redirect to login.
@@ -117,7 +82,7 @@ export class CognitoService {
 
   updateToken(): Observable<any> {
     return new Observable((observer) => {
-      Auth.currentSession()
+      this.refreshAndObtainAwsCognitoUserSession()
         .then((refreshed) => {
           this.cognitoAuthToken = this.parseToken(refreshed);
           observer.next();
@@ -137,7 +102,7 @@ export class CognitoService {
   }
 
   public async login() {
-    await Auth.federatedSignIn();
+    await signInWithRedirect();
   }
 
   public async logout() {
@@ -146,7 +111,7 @@ export class CognitoService {
       this.fakeUser = null;
       return;
     }
-    await Auth.signOut();
+    await signOut();
   }
 
   public getUser() {
@@ -167,10 +132,90 @@ export class CognitoService {
     return user;
   }
 
-  public getToken(): any {
+  public getToken() {
     if (!this.awsCognitoConfig.enabled) {
       return JSON.stringify(this.fakeUser);
     }
     return this.cognitoAuthToken;
   }
+
+  private async loadRemoteConfig() {
+    let url: string = this.configService.getApiBasePath() + "/api/awsCognitoConfig";
+    this.awsCognitoConfig = await lastValueFrom(
+      this.http.get(url, { observe: "body", responseType: "json" })
+    ) as AwsCognitoConfig;
+
+    const amplifyConfig = this.toAmplifyConfig(this.awsCognitoConfig);
+    console.log("Using amplify config = " + JSON.stringify(amplifyConfig));
+    Amplify.configure(amplifyConfig);
+  }
+
+  private toAmplifyConfig(awsCognitoConfig: AwsCognitoConfig): ResourcesConfig {
+    // conversion to config aws-amplify (gen 2)
+    return {
+      Auth: {
+        Cognito: {
+          userPoolClientId: awsCognitoConfig.aws_user_pools_web_client_id,
+          userPoolId: awsCognitoConfig.aws_user_pools_id,
+          loginWith: {
+            oauth: {
+              domain: awsCognitoConfig.oauth.domain,
+              scopes: ['openid'],
+              redirectSignIn: [awsCognitoConfig.oauth.redirectSignIn],
+              redirectSignOut: [this.awsCognitoConfig.oauth.redirectSignOut],
+              responseType: 'code',
+            },
+          }
+        }
+      }
+    }
+  }
+
+  private getParameterByName(name) {
+    const url = window.location.href;
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    const results = regex.exec(url);
+    if (!results) {
+      return null;
+    }
+    if (!results[2]) {
+      return "";
+    }
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  /**
+   * See OIDC Attribute Mapping mapping reference:
+   *      https://github.com/bcgov/nr-forests-access-management/wiki/OIDC-Attribute-Mapping
+   * The display username is "custom:idp_username" from token.
+   */
+  private parseToken(authToken: CognitoUserSession): CognitoAuthToken {
+    console.log("authToken: ", authToken)
+    const decodedIdToken = authToken.getIdToken().decodePayload();
+    const decodedAccessToken = authToken.getAccessToken().decodePayload();
+    return {
+      id_token: decodedIdToken,
+      access_token: decodedAccessToken,
+      jwtToken: authToken
+    };
+  }
+
+  /**
+   * Note:
+   * `aws-amplify` (v6) does not expose previous CognitoUserSession (JWT Token) and it does not
+   * expose 'refreshToken' anymore than previous v5.
+   * To get entire encoded token, calls to the 'toString()' is necessary (but is not documented 
+   * on aws-amplify).
+   * @returns newly fetched session converted into Promise<CognitoUserSession> type.
+   */
+  private async refreshAndObtainAwsCognitoUserSession(): Promise<CognitoUserSession> {
+    const authSession = await fetchAuthSession({ forceRefresh: true });
+    const cognitoUserSession = new CognitoUserSession( {
+      IdToken: new CognitoIdToken({IdToken: authSession.tokens.idToken.toString()}),
+      AccessToken: new CognitoAccessToken({AccessToken: authSession.tokens.accessToken.toString()}),
+      // RefreshToken: /* aws-amplify v6 does not provide refreshToken anymore, only internally for aws-amplify*/
+    })
+    return cognitoUserSession;
+  }
+
 }
